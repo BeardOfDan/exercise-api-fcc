cbc011ca5a0ea9e110e31bced7686d75063940b2
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-20 20:19:38 -0700

    (feat) Create server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res, next) => {
+  res.send('Welcome to the Exercise Tracker API');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
