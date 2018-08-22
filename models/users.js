0929d59faf910a4556c3e49bf14512ccac56780b
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-22 11:08:06 -0700

    (feat) Create users model

---
+++ models/users.js
@@ -0,0 +1,22 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const { maxLength } = require('../config/keys');
+
+module.exports = {
+  'User': mongoose.model('User', new Schema({
+    'username': {
+      'type': String,
+      'required': true,
+      'unique': true,
+      'maxlength': [maxLength, 'username is too long']
+    },
+    '_id': {
+      'type': String,
+      'index': true,
+      'unique': true,
+      'default': mongoose.Types.ObjectId
+    }
+  }))
+};
