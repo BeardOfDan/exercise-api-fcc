59339f86541118ccf3e667d86877ddb723cd794c
Daniel Roberts <BeardOfDan@gmail.com>
2018-08-22 15:40:09 -0700

    (feat) Setup mongoose connection
    Also have an initial run of the models code, and refactored
    routes/api.js to routes/apiRoutes.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express');
-
-const apiRoutes = require('./routes/api');
-
 const app = express();
 
 app.use(express.json());
 
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 5000;
+const KEYS = require('./config/keys');
+
+mongoose.connect(KEYS.mongoURI);
+
+// Run models code
+require('./models/user');
+require('./models/exercise');
 
 app.get('/', (req, res, next) => {
   res.send('Welcome to the Exercise Tracker API');
 });
 
-apiRoutes(app);
+require('./routes/apiRoutes')(app);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
